9fd64dca81dcc89497b7731ab4b99a47d05001c2
Alejandro <104107373+alejandro-atala@users.noreply.github.com>
2022-05-23 21:27:40 -0300

    Multiplicación Modifique multiplicación de arrays

--- Multiplicacion/index.ts
+++ Multiplicacion/index.ts
@@ -1,22 +1,42 @@
-let array1: number[] = [1, 2, 3];
-let array2: number[] = [4, 5, 6];
-let array3: number[] = [2, 1, 2];
-let array4: number[] = [1, 2, 1];
+let array1: number[] = new Array(3);
+let array2: number[] = new Array(3);
+let array3: number[] = new Array(3);
+let array4: number[] = new Array(3);
 let resultado: number[] = new Array(3);
 let i = 0;
 
-let multiplicarArreglo = (
+let cargarArray = (
   array_1: number[],
   array_2: number[],
   array_3: number[],
   array_4: number[]
-): number[] => {
+) => {
   for (i = 0; i < 3; i++) {
-    resultado[i] = array_1[i] * array_2[i] * array_3[i] * array_4[i];
+    array_1[i] = Number(prompt("Ingrese numeros en array 1"));
+  }
+  for (i = 0; i < 3; i++) {
+    array_2[i] = Number(prompt("Ingrese numeros en array 2"));
+  }
+  for (i = 0; i < 3; i++) {
+    array_3[i] = Number(prompt("Ingrese numeros en array 3"));
+  }
+  for (i = 0; i < 3; i++) {
+    array_4[i] = Number(prompt("Ingrese numeros en array 4"));
+  }
+};
+
+let multiplicarArreglo = (array_A: number[], array_B: number[]): number[] => {
+  for (i = 0; i < 3; i++) {
+    resultado[i] = array_A[i] * array_B[i];
   }
   return resultado;
 };
 
-console.log(multiplicarArreglo(array1, array2, [1, 1, 1], [1, 1, 1]));
+cargarArray(array1, array2, array3, array4);
 
-console.log(multiplicarArreglo(array1, array2, array3, array4));
+multiplicarArreglo(array1, array2);
+console.log(resultado);
+multiplicarArreglo(resultado, array3);
+console.log(resultado);
+multiplicarArreglo(resultado, array4);
+console.log(resultado);
