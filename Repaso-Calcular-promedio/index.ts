787b6ce28aa329952d277b14ea2362630119834b
Alejandro <104107373+alejandro-atala@users.noreply.github.com>
2022-05-27 20:06:48 -0300

    Repaso promedio Lo hice con array

--- Repaso-Calcular-promedio/index.ts
+++ Repaso-Calcular-promedio/index.ts
@@ -1,27 +1,27 @@
 let jugadores: number = Number(prompt("Ingrese cantidad de jugadores"));
-let edades: number = 0;
+let edades: number[] = new Array(jugadores);
 let promedio: number = 0;
-
+let edad = 0;
+//----------------------------------------------------------
 function Aleatorio(min: number, max: number): number {
-  edades = Math.floor(Math.random() * (max - min + 1)) + min;
-  return edades;
+  edad = Math.floor(Math.random() * (max - min + 1)) + min;
+  return edad;
 }
-
+//--------------------------------------------------------
 let i: number = 0;
-
-function Promedio(cantidad: number): number {
+//----------------------------------------------------------
+function Promedio(cantidad: number, edades: number[]): number {
   let suma: number = 0;
 
   for (i = 0; i < cantidad; i++) {
-    Aleatorio(3, 7);
-    suma = suma + edades;
-    console.log(edades);
+    suma = suma + Aleatorio(3, 7);
+    console.log(edad);
   }
   promedio = suma / cantidad;
   return promedio;
 }
-
-Promedio(jugadores);
+//-------------------------------------------------------------
+Promedio(jugadores, edades);
 console.log(
   "El promedio de edad de los jugadores es de : " +
     promedio.toFixed(1) +
