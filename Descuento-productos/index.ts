603a7b93c6432edf774f19030461d9bd844ed32d
Alejandro <104107373+alejandro-atala@users.noreply.github.com>
2022-06-10 21:26:36 -0300

    Descuento

---
+++ Descuento-productos/index.ts
@@ -0,0 +1,33 @@
+let cantidadProductos: number = Number(prompt("Ingrese cantidad de productos"));
+
+let arrayProductos: number[] = new Array(cantidadProductos);
+//------------------------------------------------------------------
+function cargarArray(array: number[], longitud: number) {
+  let i: number = 0;
+  for (i = 0; i < longitud; i++) {
+    array[i] = Number(prompt("Ingrese valores " + (i + 1)));
+  }
+  console.log(array);
+}
+//--------------------------------------------------------------------
+function calcularCompra(array: number[], cantidad: number) {
+  let i: number = 0;
+  let suma: number = 0;
+  for (i = 0; i < cantidad; i++) {
+    suma += array[i];
+  }
+  if (suma > 3000 && suma <= 5000) {
+    suma = suma * 0.9;
+    console.log("Usted tiene descuento del 10%, su compra es $ " + suma);
+  } else if (suma > 5000) {
+    suma = suma * 0.8;
+    console.log("Usted tiene descuento del 20%, su compra es $ " + suma);
+  } else {
+    console.log("Usted no tiene descuento, su compra es $ " + suma);
+  }
+}
+//--------------------------------------------------------------------
+
+cargarArray(arrayProductos, cantidadProductos);
+
+calcularCompra(arrayProductos, cantidadProductos);
